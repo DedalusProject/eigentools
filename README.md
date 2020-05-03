f457afc3193c32c16fdff94cdad962d07edea56b
J. S. Oishi <jsoishi@gmail.com>
2020-05-03 16:25:37 -0400

    eigentools is now pip-installable.
    * this closes issue #2

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 Eigentools is a set of tools for studying linear eigenvalue problems. The underlying eigenproblems are solved using [Dedalus](http://dedalus-project.org).
 
 Eigentools was developed with support from the Research Corporation under award Scialog Collaborative Award (TDA) ID# 24231.
+
+## Developers
+The core development team consists of 
+
+* Jeff Oishi (<jsoishi@gmail.com>)
+* Evan Anders (<Evan.Anders@colorado.edu>)
+* Susan Clark (<susanclark19@gmail.com>)
+* Ben Brown (<bpbrown@gmail.com>)
+* Keaton Burns (<keaton.burns@gmail.com>)
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="eigentools", 
+    version="1.0",
+    author="J. S. Oishi",
+    author_email="jsoishi@gmail.com",
+    description="A toolkit for solving eigenvalue problems with Dedalus",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dedalusproject/eigentools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    ],
+    python_requires='>=3.5',
+)
