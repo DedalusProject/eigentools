67d15f33d488a9b6715034ec3e926b82fac2c67d
J. S. Oishi <jsoishi@gmail.com>
2020-11-30 09:54:05 -0500

    Updated Orr-Somerfeld tests
    * uses critical polisher
    * reports L2 norm against Orszag '71

--- examples/orr_sommerfeld.py
+++ examples/orr_sommerfeld.py
@@ -17,7 +17,8 @@ import dedalus.public as de
 import numpy as np
 import matplotlib.pylab as plt
 import sys
-
+import logging
+logger = logging.getLogger(__name__.split('.')[-1])
 
 file_name = sys.argv[0].strip('.py')
 comm = MPI.COMM_WORLD
@@ -53,8 +54,8 @@ cf = CriticalFinder(EP,("Re", "alpha"), comm, find_freq=True)
 start = time.time()
 nx = 20
 ny = 20
-xpoints = np.linspace(5500, 6500, nx)
-ypoints = np.linspace(0.95, 1.15, ny)
+xpoints = np.linspace(5500, 6000, nx)
+ypoints = np.linspace(1.0, 1.1, ny)
 try:
     cf.load_grid('{}.h5'.format(file_name))
 except:
@@ -63,16 +64,28 @@ except:
         cf.save_grid(file_name)
 end = time.time()
 if comm.rank == 0:
-    print("grid generation time: {:10.5f} sec".format(end-start))
+    logger.info("grid generation time: {:10.5f} sec".format(end-start))
 
-crit = cf.crit_finder()
-#crit = cf.critical_polisher(method='Nelder-Mead')
+crit = cf.critical_polisher(tol=1e-5, method='Nelder-Mead')
+
+Re_orszag = 5772.22
+alpha_orszag = 1.02056
+omega_orszag = -1555.2070
 
 if comm.rank == 0:
-    print("crit = {}".format(crit))
-    print("critical wavenumber alpha = {:10.5f}".format(crit[0]))
-    print("critical Re = {:10.5f}".format(crit[1]))
-    print("critical omega = {:10.5f}".format(crit[2]))
+    alpha = crit[0]
+    Re = crit[1]
+    omega = crit[2]
+
+    Re_err = (Re-Re_orszag)/Re_orszag
+    alpha_err = (alpha-alpha_orszag)/alpha_orszag
+    L2 = np.sqrt((Re-Re_orszag)**2 + (alpha-alpha_orszag)**2)
+    logger.info("critical wavenumber alpha = {:10.5f}".format(alpha))
+    logger.info("critical Re = {:10.5f}".format(Re))
+    logger.info("critical omega = {:10.5f}".format(omega))
+    logger.info("critical Re error = {:10.5e}".format(Re_err))
+    logger.info("critical alpha error = {:10.5}".format(alpha_err))
+    logger.info("L2 norm from Orszag 71 solution = {:10.5e}".format(L2))
 
     cf.save_grid('orr_sommerfeld_growth_rates')
     cf.plot_crit()
